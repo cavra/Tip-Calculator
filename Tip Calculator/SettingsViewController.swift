ac0f1eacb5fb4cd2045275a49344235a0a622dd1
Arimotamo <arimotamo@gmail.com>
2015-12-11 22:35:44 -0600

    second commit

--- Tip Calculator/SettingsViewController.swift
+++ Tip Calculator/SettingsViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
     
     @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    @IBOutlet weak var colorPicker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //connect the data
+        colorPicker.delegate = self
+        colorPicker.dataSource = self
+        
+        //fill the array
+        pickerData = ["Day", "Night", "Red", "Green", "Blue", "Yellow"]
+        
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -26,6 +35,14 @@ class SettingsViewController: UIViewController {
         if (defaults.objectForKey("default_tip") != nil) {
             let defaultSegIndex = defaults.integerForKey("default_tip")
             defaultTipControl.selectedSegmentIndex = defaultSegIndex
+        }
+        if (defaults.objectForKey("default_color") != nil) {
+            let newColor = defaults.objectForKey("default_color") as! String
+            changeColorTheme(newColor)
+        }
+        if (defaults.objectForKey("default_row") != nil) {
+            let defaultRow = defaults.integerForKey("default_row")
+            colorPicker.selectRow(defaultRow, inComponent: 0, animated: false)
         }
         
     }
@@ -41,6 +58,65 @@ class SettingsViewController: UIViewController {
         let defaults = NSUserDefaults.standardUserDefaults()
         defaults.setInteger(defaultTipControl.selectedSegmentIndex, forKey: "default_tip")
         defaults.synchronize()
+    }
+    
+    func changeColorTheme(newColor: String) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(newColor, forKey: "default_color")
+        defaults.synchronize()
+        
+        //change background and tint colors
+        if (newColor == "Day") {
+            self.view.backgroundColor = UIColor.whiteColor()
+            self.view.tintColor = UIColor.blueColor()
+            defaults.setObject(0, forKey: "default_row")
+        }
+        else if (newColor == "Night") {
+            self.view.backgroundColor = UIColor.grayColor()
+            self.view.tintColor = UIColor.blackColor()
+            defaults.setObject(1, forKey: "default_row")
+        }
+        else if (newColor == "Red") {
+            self.view.backgroundColor = UIColor(red: 1, green: 0.6, blue: 0.4, alpha: 1)
+            self.view.tintColor = UIColor.blackColor()
+            defaults.setObject(2, forKey: "default_row")
+        }
+        else if (newColor == "Green") {
+            self.view.backgroundColor = UIColor(red: 0.61, green: 1, blue: 0.46, alpha: 1)
+            self.view.tintColor = UIColor.blackColor()
+            defaults.setObject(3, forKey: "default_row")
+        }
+        else if (newColor == "Blue") {
+            self.view.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1)
+            self.view.tintColor = UIColor.blueColor()
+            defaults.setObject(4, forKey: "default_row")
+        }
+        else if (newColor == "Yellow") {
+            self.view.backgroundColor = UIColor(red: 1, green: 1, blue: 0.4, alpha: 1)
+            self.view.tintColor = UIColor.blackColor()
+            defaults.setObject(5, forKey: "default_row")
+        }
+        else {
+            changeColorTheme("Day")
+        }
+    }
+    
+    //MARK: - Delegates and data sources for colorPicker
+    //data sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        changeColorTheme(pickerData[row])
     }
     
     /*
--- Tip Calculator/TipViewController.swift
+++ Tip Calculator/TipViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalTwoLabel: UILabel!
+    @IBOutlet weak var totalThreeLabel: UILabel!
+    @IBOutlet weak var totalFourLabel: UILabel!
+    @IBOutlet weak var totalFiveLabel: UILabel!
+    
     @IBOutlet weak var tipControl: UISegmentedControl!
     
     override func viewDidLoad() {
@@ -21,25 +26,63 @@ class ViewController: UIViewController {
         //default the text labels to $0.00
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        totalTwoLabel.text = "$0.00"
+        totalThreeLabel.text = "$0.00"
+        totalFourLabel.text = "$0.00"
+        totalFiveLabel.text = "$0.00"
         
+        //raise the decimal pad as soon as it opens
+        billField.becomeFirstResponder()
+        
+        //write the placeholder text
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        billField.attributedPlaceholder = NSAttributedString(string: formatter.stringFromNumber(0)!)
+        
+        //remember the bill from a recent* previous session (60*10 = 10 minutes)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if (defaults.objectForKey("default_bill") != nil) {
+            let then = defaults.objectForKey("time_at_close")!
+            if (NSDate().timeIntervalSinceReferenceDate - then.timeIntervalSinceReferenceDate < 60*10) {
+                billField.text = defaults.objectForKey("default_bill") as! String!
+            }
+        }
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        
         //set the defaultTipIndex to 0, in case the actual default doesn't exist
         var defaultTipIndex = 0
         
-        //get the user-defined default from the settings viewController
+        //get the user-defined defaults 
         let defaults = NSUserDefaults.standardUserDefaults()
         if (defaults.objectForKey("default_tip") != nil) {
             defaultTipIndex = defaults.integerForKey("default_tip")
+        }
+        if (defaults.objectForKey("default_color") != nil) {
+            let newColor = defaults.objectForKey("default_color") as! String
+            changeColorTheme(newColor)
+        }
+        else {
+            changeColorTheme("Day")
         }
    
         //set the segment control to reflect the defaulted value
         tipControl.selectedSegmentIndex = defaultTipIndex
         
         updateBill(defaultTipIndex)
+        
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        //record the text in the bill field and the current time
+        let defaults = NSUserDefaults.standardUserDefaults()
+
+        defaults.setObject(billField.text, forKey: "default_bill")
+        defaults.setObject(NSDate(), forKey: "time_at_close")
+
+        defaults.synchronize()
     }
     
     override func didReceiveMemoryWarning() {
@@ -56,26 +99,67 @@ class ViewController: UIViewController {
     }
     
     func updateBill(tipPercentageIndex: Int) {
-        //access any stored data
-        let defaults = NSUserDefaults.standardUserDefaults()
-        
         let tipPercentages = [0.15, 0.2, 0.25]
         let tipPercentage = tipPercentages[tipPercentageIndex]
         let billAmount = NSString(string: billField.text!).doubleValue
         
-        //calculate the tip
+        //calculate the tip and total
         let tip = billAmount * tipPercentage
         let total = billAmount + tip
+        let totalForTwo = total / 2
+        let totalForThree = total / 3
+        let totalForFour = total / 4
+        let totalForFive = total / 5
         
-        //set and format text labels
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-    
-        //synchronize the data
-        defaults.synchronize()
+        //set and format text labels using locale data
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+
+        tipLabel.text = formatter.stringFromNumber(tip)
+        totalLabel.text = formatter.stringFromNumber(total)
+        totalTwoLabel.text = formatter.stringFromNumber(totalForTwo)
+        totalThreeLabel.text = formatter.stringFromNumber(totalForThree)
+        totalFourLabel.text = formatter.stringFromNumber(totalForFour)
+        totalFiveLabel.text = formatter.stringFromNumber(totalForFive)
+        
     }
     
+    //@todo
+    //having this function in here AND in
+    //settingsViewController is bad implementation
+    func changeColorTheme(newColor: String) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(newColor, forKey: "default_color")
+        defaults.synchronize()
+        
+        //change background and tint colors
+        if (newColor == "Day") {
+            self.view.backgroundColor = UIColor.whiteColor()
+            self.view.tintColor = UIColor.blueColor()
+        }
+        else if (newColor == "Night") {
+            self.view.backgroundColor = UIColor.grayColor()
+            self.view.tintColor = UIColor.blackColor()
+        }
+        else if (newColor == "Red") {
+            self.view.backgroundColor = UIColor(red: 1, green: 0.6, blue: 0.4, alpha: 1)
+            self.view.tintColor = UIColor.blackColor()
+        }
+        else if (newColor == "Green") {
+            self.view.backgroundColor = UIColor(red: 0.61, green: 1, blue: 0.46, alpha: 1)
+            self.view.tintColor = UIColor.blackColor()
+        }
+        else if (newColor == "Blue") {
+            self.view.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1)
+            self.view.tintColor = UIColor.blueColor()
+        }
+        else if (newColor == "Yellow") {
+            self.view.backgroundColor = UIColor(red: 1, green: 1, blue: 0.4, alpha: 1)
+            self.view.tintColor = UIColor.blackColor()
+        }
+        else {
+            changeColorTheme("Day")
+        }
+    }
 }
 
